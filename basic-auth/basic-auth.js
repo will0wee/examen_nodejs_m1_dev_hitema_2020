80ceff75c1b928521aeacab466996ebfcf40b27b
Damien POPIEL <damien.popile@gmail.com>
2020-05-05 14:37:19 +0200

    step 1 : basic-auth

--- basic-auth/basic-auth.js
+++ basic-auth/basic-auth.js
@@ -1,9 +1,19 @@
 const crypto = require('crypto');
+const hash = crypto.createHash('sha1');
 
 function sha1Encode(data) {
-    // To be implemented!
+	hash.update(data);
+	return hash.digest('hex');
 }
 
 module.exports.digestAuth = (request, response, next) => {
-    // To be implemented!
+    const authorization = request.headers.authorization;
+    const encoded = authorization.replace('Basic ', '');
+    const decoded = Buffer.from(encoded, 'base64').toString('utf8');
+    const authentication = decoded.split(':');
+
+    const isValid = authentication[0] === sha1Encode('node')
+        && authentication[1] === sha1Encode('password');
+    
+    isValid ? next() : response.sendStatus(401);
 }
